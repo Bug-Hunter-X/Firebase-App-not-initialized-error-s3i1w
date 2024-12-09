a89afb12559586fb634488c284a354f61e886c55
BugHunter-X <askzenai@gmail.com>
2024-12-09 08:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase SDK may throw an error if the app is not properly initialized before attempting to access Firebase services. This can happen if the `initializeApp` function is called too late in the app's lifecycle or if there is an issue with the Firebase configuration.
